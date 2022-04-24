0f18e7e5303dcbe009a7244298b748a2ce85b16e
Juan M Godoy <juanmanuelgodoy2@gmail.com>
2022-04-24 02:58:41 -0300

    acerca de primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { LogoArgProgramaComponent } from './componentes/logo-arg-programa/logo-arg-programa.component';
 import { SocialesComponent } from './componentes/sociales/sociales.component';
 import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './componentes/banner/banner.component';
     HeaderComponent,
     LogoArgProgramaComponent,
     SocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
